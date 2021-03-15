67a366dbe712d3be5ff8c66c403c1879f76be178
akira-hiroshima <77510760+akira-hiroshima@users.noreply.github.com>
2021-03-15 17:58:37 +0300

    feat: end human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function toReadable (number) {
-  
+  let ones = ["", "one","two","three", "four", "five", "six", "seven", "eight", "nine",
+  "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+  let tens = ["", "", "twenty ", "thirty ", "forty ", "fifty ", "sixty ", "seventy ", "eighty ", "ninety "];
+  let answer;
+  let str = String(number).split("");
+  if (number == 0) {return 'zero';}
+  if (number < 20) {return ones[number];}
+  if (number < 100) {
+	  answer = tens[str[0]] + ones[str[1]];
+	  return answer.trim();
+	  }
+  if (number < 1000) {
+	  if (str[1]==1) {return answer = ones[str[0]]+' hundred ' + ones[str[1]+str[2]] }
+	  answer = ones[str[0]]+' hundred '+ tens[str[1]] + ones[str[2]];
+	  return answer.trim();
+	  }
 }
+  
